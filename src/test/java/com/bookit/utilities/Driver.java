cafd0cac5eecd2bf0673055f3a2fa5d8ec13b0bf
AndreaKrizbai <krizbai.andrea@gmail.com>
2020-06-07 16:23:48 -0400

    browser stack testing

--- src/test/java/com/bookit/utilities/Driver.java
+++ src/test/java/com/bookit/utilities/Driver.java
@@ -9,8 +9,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
 import java.net.URL;
 public class Driver {
+    public static final String USERNAME = "andreacybertek1";
+    public static final String AUTOMATE_KEY = "dFzQkYy4QTpX7YYyGrb6";
+    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
     //same for everyone
     private static final ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
     //so no one can create object of Driver class
@@ -26,7 +31,8 @@ public class Driver {
      */
     public synchronized static WebDriver getDriver() {
         //String GRID_URL = "http://35.171.158.59:4444/wd/hub";
-        String GRID_URL = "http://3.236.14.239:4444/wd/hub";
+        //String GRID_URL = "http://3.236.14.239:4444/wd/hub";
+        String GRID_URL = "http://34.204.195.171:4444/wd/hub";
         //if webdriver object doesn't exist
         //create it
         if (driverPool.get() == null) {
@@ -107,6 +113,74 @@ public class Driver {
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driverPool.set(new FirefoxDriver());
+                    break;
+                case "browser-stack-chrome":
+                    DesiredCapabilities caps = new DesiredCapabilities();
+                    caps.setCapability("browser", "Chrome");
+                    caps.setCapability("browser_version", "83.0");
+                    caps.setCapability("os", "Windows");
+                    caps.setCapability("os_version", "10");
+                    caps.setCapability("resolution", "1920x1080");
+                    caps.setCapability("name", "BookIT Automation");
+                    try {
+                        driverPool.set(new RemoteWebDriver(new URL(URL), caps));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+//                case "browser-stack-chrome":
+//                    DesiredCapabilities caps3 = new DesiredCapabilities();
+//                    caps3.setCapability("browser", "Chrome");
+//                    caps3.setCapability("browser_version", "83.0");
+//                    caps3.setCapability("os", "OS X");
+//                    caps3.setCapability("os_version", "Catalina");
+//                    caps3.setCapability("resolution", "1024x768");
+//                    caps3.setCapability("name", "BookIT Automation with Mac");
+//                    try {
+//                        driverPool.set(new RemoteWebDriver(new URL(URL), caps3));
+//                    } catch (MalformedURLException e) {
+//                        e.printStackTrace();
+//                    }
+//                    break;
+                case "browser-stack-safari":
+                    DesiredCapabilities caps2 = new DesiredCapabilities();
+                    caps2.setCapability("browser", "Safari");
+                    caps2.setCapability("browser_version", "11.1");
+                    caps2.setCapability("os", "OS X");
+                    caps2.setCapability("os_version", "High Sierra");
+                    caps2.setCapability("resolution", "1024x768");
+                    caps2.setCapability("name", "Bstack-[Java] Sample Test BOOKIT");
+                    try {
+                        driverPool.set(new RemoteWebDriver(new URL(URL), caps2));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "browser-stack-ios":
+                    DesiredCapabilities capsIOS = new DesiredCapabilities();
+                    capsIOS.setCapability("browserName", "iPhone");
+                    capsIOS.setCapability("device", "iPhone 11 Pro Max");
+                    capsIOS.setCapability("realMobile", "true");
+                    capsIOS.setCapability("os_version", "13");
+                    capsIOS.setCapability("name", "BookIT Automation");
+                    try {
+                        driverPool.set(new RemoteWebDriver(new URL(URL), capsIOS));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case "browser-stack-android":
+                    DesiredCapabilities capsAndroid = new DesiredCapabilities();
+                    capsAndroid.setCapability("browserName", "android");
+                    capsAndroid.setCapability("device", "Samsung Galaxy S20 Ultra");
+                    capsAndroid.setCapability("realMobile", "true");
+                    capsAndroid.setCapability("os_version", "10.0");
+                    capsAndroid.setCapability("name", "BookIT Automation");
+                    try {
+                        driverPool.set(new RemoteWebDriver(new URL(URL), capsAndroid));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     throw new RuntimeException("Wrong browser name!");
